60765b9144988cac9de5382b3b2157ae6d2d3415
Sahil Thakur <151663130+sahilthakur2251@users.noreply.github.com>
2026-09-05 00:01:17 +0530

    Update CORS origin to include new frontend URL

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import routes from "./src/routes/index.route.js";
 const app = express();
 
 app.use(cors({
-    origin: ["https://mern-frontend-puwpj5yoy-sahil-thakur-1s-projects.vercel.app", "http://localhost:5173"],
+    origin: ["https://mern-frontend-puwpj5yoy-sahil-thakur-1s-projects.vercel.app", "https://mern-frontend-two-phi.vercel.app", "http://localhost:5173"],
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
     allowedHeaders: [
         "Content-Type",
